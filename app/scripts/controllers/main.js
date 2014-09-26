86e6115cb145723c08f0be07294d51953fc10919
Franck Magnan <fmagnan@gmail.com>
2014-09-26 11:41:19 +0200

    retrieve all posts to apply filters on them

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,11 +1,27 @@
 'use strict';
 
 angular.module('novaApp').controller('MainCtrl', function ($scope, $http, apiEndPoint) {
-    var uri = apiEndPoint + '/posts';
-    $http.get(uri).success(function (response) {
+    $scope.generateRandomColor = function () {
+        var letters = '0123456789ABCDEF'.split('');
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    };
+
+    $scope.filters = {};
+
+    $http.get(apiEndPoint + '/posts').success(function (response) {
         $scope.posts = response.data;
     });
+
     $http.get(apiEndPoint + '/feeds').success(function (response) {
-        $scope.feeds = response.data;
+        var feeds = response.data;
+        $scope.feeds = {};
+        for (var i= 0 ; i < feeds.length; i++) {
+            $scope.feeds[feeds[i].id] = feeds[i];
+            $scope.feeds[feeds[i].id]['color'] = $scope.generateRandomColor();
+        }
     });
 });
