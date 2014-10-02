51aebb2f9e74da71e5d862aaff9efe86c3c06aaf
Franck Magnan <fmagnan@gmail.com>
2014-10-02 18:09:22 +0200

    z

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,5 +1,21 @@
 'use strict';
 
+angular.module('nova').filter('includesInFeed', function () {
+    return function (items, feedIdFilter) {
+        if (typeof feedIdFilter === 'undefined') {
+            return items;
+        }
+        var filtered = [];
+        for (var i = 0; i < items.length; i++) {
+            var item = items[i];
+            if (item.feedId === feedIdFilter) {
+                filtered.push(item);
+            }
+        }
+        return filtered;
+    }
+});
+
 angular.module('nova').controller('MainCtrl', function ($scope, $http, apiEndPoint, Pagination) {
 
     $scope.postsPagination = new Pagination();
