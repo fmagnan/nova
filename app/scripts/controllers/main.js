16cea32fc8555135168d112be076272444814360
Franck Magnan <fmagnan@gmail.com>
2014-10-02 16:42:24 +0200

    trying to use filters

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,22 @@ angular.module('nova').controller('MainCtrl', function ($scope, $http, apiEndPoi
         $scope.feeds = response.data;
     });
 
-    $scope.removePost = function(position) {
+    $scope.removePost = function (position) {
         $scope.postsPagination.items.splice(position, 1);
+    };
+
+    $scope.getPosts = function () {
+        console.log('get posts');
+        return $scope.postsPagination.get();
+    };
+
+    $scope.selectFeed = function (feedId) {
+        $scope.feedIdFilter = feedId;
+        /*var i = $scope.postsPagination.items.length;
+         while (i--) {
+         if ($scope.postsPagination.items[i]['feedId'] != feedId) {
+         $scope.postsPagination.items.splice(i, 1);
+         }
+         }*/
     };
 });
--- app/scripts/services/pagination.js
+++ app/scripts/services/pagination.js
@@ -4,44 +4,17 @@ angular.module('nova').factory('Pagination', function ($http, apiEndPoint) {
     var Pagination = function () {
         this.items = [];
         this.isLoading = false;
-        this.params = {'limit': 10, 'offset': 0};
+        this.offset = 0;
         this.hasNext = true;
     };
 
-    Pagination.prototype.reset = function () {
-        this.items = [];
-        this.isLoading = false;
-        this.params.offset = 0;
-        this.hasNext = true;
-    };
-
-    Pagination.prototype.buildHttpQueryParams = function (obj) {
-        var str = [];
-        for (var p in obj) {
-            if (obj.hasOwnProperty(p)) {
-                str.push(encodeURIComponent(p) + '=' + encodeURIComponent(obj[p]));
-            }
-        }
-        return str.join('&');
-    };
-
-    Pagination.prototype.get = function (conditions, isResetNeeded) {
-        if (true === isResetNeeded) {
-            this.reset();
-        }
-
+    Pagination.prototype.get = function () {
         if (this.isLoading) {
             return;
         }
         this.isLoading = true;
 
-        if (typeof conditions !== 'undefined') {
-            for (var key in conditions) {
-                this.params[key] = conditions[key];
-            }
-        }
-
-        var url = apiEndPoint + '/posts?' + this.buildHttpQueryParams(this.params);
+        var url = apiEndPoint + '/posts?limit=10&offset=' + this.offset;
         $http.get(url).success(function (response) {
             if (0 === response.data.length) {
                 this.isLoading = true;
@@ -50,7 +23,7 @@ angular.module('nova').factory('Pagination', function ($http, apiEndPoint) {
                 for (var i = 0; i < response.data.length; i++) {
                     this.items.push(response.data[i]);
                 }
-                this.params.offset = this.items.length;
+                this.offset = this.items.length;
                 this.isLoading = false;
             }
         }.bind(this));
