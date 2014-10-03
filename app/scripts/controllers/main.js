f7b723b7386c4bc236eb90bcba31c3f3e828ef06
Franck Magnan <fmagnan@gmail.com>
2014-10-03 14:47:08 +0200

    fixed toggle feed selection function

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,13 +2,13 @@
 
 angular.module('nova').filter('filtersByFeeds', function () {
     return function (items, selectedFeeds) {
-        if (typeof selectedFeeds === 'undefined') {
+        if (selectedFeeds.length === 0) {
             return items;
         }
         var filtered = [];
         for (var i = 0; i < items.length; i++) {
             var item = items[i];
-            if (typeof selectedFeeds[item.feedId] === 'undefined') {
+            if (selectedFeeds.indexOf(item.feedId) === -1) {
                 filtered.push(item);
             }
         }
@@ -18,41 +18,36 @@ angular.module('nova').filter('filtersByFeeds', function () {
 
 angular.module('nova').controller('MainCtrl', function ($scope, $http, apiEndPoint, Pagination) {
 
+    $scope.removePost = function (position) {
+        $scope.postsPagination.items.splice(position, 1);
+    };
+
+    $scope.toggleFeed = function (feedId) {
+        var position = $scope.selectedFeeds.indexOf(feedId);
+        if (position === -1) {
+            $scope.selectedFeeds.push(feedId);
+        } else {
+            $scope.selectedFeeds.splice(position, 1);
+        }
+    };
+
+    $scope.resetFiltersOnFeeds = function () {
+        $scope.selectedFeeds = [];
+    };
+
+    $scope.isFeedSelected = function (feedId) {
+        return $scope.selectedFeeds.indexOf(feedId) === -1;
+    };
+
     $scope.title = 'Planet';
     $scope.googleAnalyticsIdentifier = 'UA-27208964-20';
+
+    $scope.resetFiltersOnFeeds();
 
     $scope.postsPagination = new Pagination();
 
     $http.get(apiEndPoint + '/feeds').success(function (response) {
         $scope.feeds = response.data;
     });
-
-    $scope.removePost = function (position) {
-        $scope.postsPagination.items.splice(position, 1);
-    };
-
-    $scope.toggleFeed = function(feedId) {
-        if (typeof $scope.selectedFeeds === 'undefined') {
-            $scope.selectedFeeds = [];
-            $scope.selectedFeeds[feedId] = feedId;
-        } else {
-            if (typeof $scope.selectedFeeds[feedId] === 'undefined') {
-                $scope.selectedFeeds[feedId] = feedId;
-            } else {
-                $scope.selectedFeeds.splice(feedId,1);
-            }
-        }
-    };
-
-    $scope.resetFiltersOnFeeds = function() {
-        $scope.selectedFeeds = undefined;
-    };
-
-    $scope.isFeedSelected = function(feedId) {
-        if (typeof $scope.selectedFeeds === 'undefined') {
-            return true;
-        }
-        return typeof $scope.selectedFeeds[feedId] === 'undefined';
-    };
 
 });
